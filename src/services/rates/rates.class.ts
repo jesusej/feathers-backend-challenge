7f93dea3439ae12311ab5ffd95ad484f3012ef04
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2025-09-02 14:49:43 -0700

    chore: Set up rates service

---
+++ src/services/rates/rates.class.ts
@@ -0,0 +1,41 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.class.html#custom-services
+import type { Params, ServiceInterface } from '@feathersjs/feathers'
+
+import type { Application } from '../../declarations'
+
+type Rates = any
+type RatesData = any
+type RatesPatch = any
+type RatesQuery = any
+
+export type { Rates, RatesData, RatesPatch, RatesQuery }
+
+export interface RatesServiceOptions {
+  app: Application
+}
+
+export interface RatesParams extends Params<RatesQuery> {}
+
+// This is a skeleton for a custom service class. Remove or add the methods you need here
+export class RatesService<ServiceParams extends RatesParams = RatesParams>
+  implements ServiceInterface<Rates, RatesData, ServiceParams, RatesPatch>
+{
+  constructor(public options: RatesServiceOptions) {}
+
+  async find(_params?: ServiceParams): Promise<Rates[]> {
+    return []
+  }
+
+  async create(data: RatesData, params?: ServiceParams): Promise<Rates>
+  async create(data: RatesData[], params?: ServiceParams): Promise<Rates[]>
+  async create(data: RatesData | RatesData[], params?: ServiceParams): Promise<Rates | Rates[]> {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current, params)))
+    }
+
+    return {
+      id: 0,
+      ...data
+    }
+  }
+}
