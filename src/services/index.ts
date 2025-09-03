a427917086a5ebbcaeac319ac109524cc2ca7c81
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2025-09-02 22:31:27 -0700

    chore: Setup report service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
+import { report } from './report/report'
 import { convert } from './convert/convert'
 import { rates } from './rates/rates'
 // For more information about this file see https://dove.feathersjs.com/guides/cli/application.html#configure-functions
 import type { Application } from '../declarations'
 
 export const services = (app: Application) => {
+  app.configure(report)
   app.configure(convert)
   app.configure(rates)
   // All services will be registered here
---
+++ src/services/report/report.ts
@@ -0,0 +1,47 @@
+// For more information about this file see https://dove.feathersjs.com/guides/cli/service.html
+
+import type { Application } from '../../declarations'
+import { ReportService, getOptions } from './report.class'
+
+export const reportPath = 'report'
+export const reportMethods: Array<keyof ReportService> = ['find', 'get', 'create', 'patch', 'remove']
+
+export * from './report.class'
+
+// A configure function that registers the service and its hooks via `app.configure`
+export const report = (app: Application) => {
+  // Register our service on the Feathers application
+  app.use(reportPath, new ReportService(getOptions(app)), {
+    // A list of all methods this service exposes externally
+    methods: reportMethods,
+    // You can add additional custom events to be sent to clients here
+    events: []
+  })
+  // Initialize hooks
+  app.service(reportPath).hooks({
+    around: {
+      all: []
+    },
+    before: {
+      all: [],
+      find: [],
+      get: [],
+      create: [],
+      patch: [],
+      remove: []
+    },
+    after: {
+      all: []
+    },
+    error: {
+      all: []
+    }
+  })
+}
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    [reportPath]: ReportService
+  }
+}
