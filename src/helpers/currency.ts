4821d1d456378aea2019e888834baacebd8adbc0
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2025-09-22 08:45:56 -0700

    feat!: Cryptocurrencies support

--- src/helpers/currency.ts
+++ src/helpers/currency.ts
@@ -1,12 +1,31 @@
+import Coingecko from '@coingecko/coingecko-typescript'
 import { Application } from '@feathersjs/feathers'
 
-/**
- * Fetches the latest exchange rates from the Exchange Rates API
- * @returns A record of currency codes and their rates
- */
-export const getLatestRates = async (app: Application): Promise<Record<string, number>> => {
+export const getLatestCryptoRates = async (app: Application): Promise<Record<string, number>> => {
+  const coingecko = new Coingecko({
+    demoAPIKey: app.get('crypto_app_id') as string,
+    environment: 'demo'
+  })
+
+  try {
+    const response = await coingecko.coins.markets.get({ vs_currency: 'usd' })
+
+    let cryptoRates: Record<string, number> = {}
+
+    response.forEach(item => {
+      cryptoRates[item.symbol?.toUpperCase() as string] = item.current_price as number
+    })
+
+    return cryptoRates
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+export const getLatestCurrencyRates = async (app: Application): Promise<Record<string, number>> => {
   const appId = app.get('currency_app_id') as string
-  return fetch(`https://openexchangerates.org/api/latest.json?app_id=${appId}&show_alternative=true`, {
+  return fetch(`https://openexchangerates.org/api/latest.json?app_id=${appId}`, {
     method: 'GET'
   })
     .then(async response => {
@@ -18,4 +37,20 @@ export const getLatestRates = async (app: Application): Promise<Record<string, n
       console.error(error)
       throw error
     })
+}
+
+/**
+ * Fetches the latest exchange rates from OpenExchangeRates and CoinGecko
+ * @returns A record of currency codes and their rates
+ */
+export const getLatestRates = async (app: Application) => {
+  const [currencyRates, cryptoRates] = await Promise.all([
+    getLatestCurrencyRates(app),
+    getLatestCryptoRates(app)
+  ])
+
+  return {
+    ...currencyRates,
+    ...cryptoRates
+  }
 }
